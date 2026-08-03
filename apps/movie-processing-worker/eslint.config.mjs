a4804003a4f3ba872cd64b32e4028b79d63d9e75
Marc Troisner <marc.troisner@gmail.com>
2026-08-03 21:31:41 +0200

    feat: placeholder worker app, database package with drizzle set up

---
+++ apps/movie-processing-worker/eslint.config.mjs
@@ -0,0 +1,4 @@
+import { config } from "@cinema-bot/eslint-config/base";
+
+/** @type {import("eslint").Linter.Config} */
+export default config;
---
+++ packages/database/src/db.ts
@@ -0,0 +1,8 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_DATABASE_URL,
+});
+
+export default drizzle({ client: pool });
